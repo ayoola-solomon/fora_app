dd59e880b23859c41f5569fdb89c8666de46e6e4
ayoolasolomon <ayoyink@yahoo.com>
2014-06-10 14:28:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForaApp::Application.config.secret_token = 'c070b94093411ab7d4235643bd9e4eb968239551fc728a017acb753e59ee3bb6dc24a0392d6787ed21c80ec2b32dec219c30a33e5d6054bbd817fa7d664b2a5e'
